42fee0764a66eaa9d74629bba5582f180eedcce6
Blair Anderson <blair81@gmail.com>
2013-05-29 22:04:07 -0600

    updated the header and the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Blogger::Application.routes.draw do
   resources :articles
   resources :author_sessions
 
-  post 'refresh' => 'authors#refresh',         :as => :refresh
-  get 'login'    => 'oauths#oauth',            :as => :login
-  get 'signup'   => 'oauths#oauth',            :as => :signup
-  get 'logout'   => 'author_sessions#destroy', :as => :logout
+  post 'refresh' => 'authors#refresh',  :as => :refresh
+  get 'login'    => 'oauths#oauth',     :as => :login
+  get 'signup'   => 'oauths#oauth',     :as => :signup
+  get 'logout'   => 'oauths#destroy',   :as => :logout
 
   match "oauth/callback" => "oauths#callback", :as => :callback
 end
