31fbabdde2839a7d2f37e2881add43681f4b07f0
Blair Anderson <blair81@gmail.com>
2013-05-29 21:18:06 -0600

    logged_in and current user needed help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 
   def logged_in?
