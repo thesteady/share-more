995a259e69aa2deb61fca8484b1d8a420b598faf
Blair Anderson <blair81@gmail.com>
2013-06-04 01:09:02 -0600

    removed old comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  #1) Should API routes be separated from the main routes?
-
-#yes. Make a namespace, it will make the api implementation more clear. 
-
-  #2) What should i be saving from each API request? something about saving the request time so that it cannot be duplicated?  
-
   def current_user
     @current_user ||= find_by_access_token || find_user_by_session
   end
